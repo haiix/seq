0db94fd1f1f34435d88e083678744d562785feda
haiix <haiix268@gmail.com>
2020-12-25 18:49:31 +0900

    Import

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
-# seq
+# seq.mjs
+
 A JavaScript module that allows iterators to use array-like methods.
+
+## Installation
+
+```javascript
+import seq from './seq.mjs'
+```
+
+## Examples
+
+1.  By passing the natural number N to the seq function, an iterator is created that increments between 0 and N.
+    You can use array-like methods (map, join, etc.) on the created iterator.
+
+    ```javascript
+    seq(3).join() // "0,1,2"
+    ```
+
+    ```javascript
+    seq(4).map(x => x + 1).join() // "1,2,3,4"
+    ```
+
+2.  You can pass an iterable object (with the [Symbol.iterator] method) to the seq function.
+
+    ```javascript
+    seq([10, 20, 30]).map(x => x + 1).join() // "11,21,31"
+    ```
+
+3.  There are no methods implemented that rewrite themselves.
+    If you want to use those methods, convert them to arrays once.
+
+    ```javascript
+    //seq(5).reverse() // Error
+    Array.from(seq(5)).reverse() // [4, 3, 2, 1, 0]
+    ```
+
+4.  In this example, the array is not created internally, and the map and filter operations for terms after 5 found in indexOf are not performed.
+
+    ```javascript
+    seq(100).map(x => x * 2).filter(x => x % 3 > 0).indexOf(16) // 5
+    ```
+
+5.  The iterator created by the seq function can be used in a for-of statement.
+    Use in processing with side effects.
+
+    ```javascript
+    for (const n of seq(3)) {
+      console.log(n) // 0, 1, 2
+    }
+    ```
+
+    ```javascript
+    for (const elem of seq(3).map(n => document.createElement('input'))) {
+      document.body.appendChild(elem)
+    }
+    ```
---
+++ seq.mjs
@@ -0,0 +1,144 @@
+/*
+ * seq.mjs
+ *
+ * Copyright (c) 2020 haiix
+ *
+ * This module is released under the MIT license:
+ * https://opensource.org/licenses/MIT
+ */
+
+export const version = '0.1.0'
+
+const toArray = ite => Array.isArray(ite) ? ite : Array.from(ite)
+
+class ArrayIterator {
+  constructor (iterator) {
+    this._ite = iterator
+  }
+
+  concat (...items) {
+    return new ArrayIterator([this._ite, ...items]).flat()
+  }
+
+  entries () {
+    return this.map((v, i) => [i, v])
+  }
+
+  every (callbackfn, thisArg = null) {
+    return !this.map(callbackfn, thisArg).map(v => !!v).includes(false)
+  }
+
+  fill (value, start = 0, end = Number.MAX_SAFE_INTEGER) {
+    if (start < 0 || end < 0) return new ArrayIterator(toArray(this._ite).fill(...arguments))
+    return this.map((v, i) => (i >= start && i < end) ? value : v)
+  }
+
+  filter (callbackfn, thisArg = null) {
+    return this.flatMap((v, i, ite) => callbackfn.call(thisArg, v, i, ite) ? [v] : [])
+  }
+
+  find (callbackfn, thisArg = null) {
+    for (const [i, v] of this.entries()) if (callbackfn.call(thisArg, v, i, this._ite)) return v
+  }
+
+  findIndex (callbackfn, thisArg = null) {
+    for (const [i, v] of this.entries()) if (callbackfn.call(thisArg, v, i, this._ite)) return i
+    return -1
+  }
+
+  flat (depth = 1) {
+    return new ArrayIterator(function * recur (ite, depth) {
+      for (const v of ite) {
+        if (depth > 0 && typeof v[Symbol.iterator] === 'function') {
+          yield * recur(v, depth - 1)
+        } else {
+          yield v
+        }
+      }
+    }(this._ite, depth))
+  }
+
+  flatMap (mapperFunction, thisArg) {
+    return this.map(mapperFunction, thisArg).flat()
+  }
+
+  forEach (callbackfn, thisArg = null) {
+    this.map(callbackfn, thisArg).fill(0).indexOf(1)
+  }
+
+  includes (searchElement, fromIndex = 0) {
+    return this.indexOf(searchElement, fromIndex) >= 0
+  }
+
+  indexOf (searchElement, fromIndex = 0) {
+    if (fromIndex < 0) return toArray(this._ite).indexOf(...arguments)
+    return this.findIndex((v, i) => i >= fromIndex && v === searchElement)
+  }
+
+  join (separator = ',') {
+    return toArray(this._ite).join(...arguments)
+  }
+
+  keys () {
+    return this.map((v, i) => i)
+  }
+
+  lastIndexOf (searchElement, fromIndex = undefined) {
+    return toArray(this._ite).lastIndexOf(...arguments)
+  }
+
+  map (callbackfn, thisArg = null) {
+    return new ArrayIterator(function * (ite, i) {
+      for (const v of ite) yield callbackfn.call(thisArg, v, ++i, ite)
+    }(this._ite, -1))
+  }
+
+  reduce (callbackfn, initialValue = undefined) {
+    let value = initialValue
+    for (const [i, v] of this.entries()) value = (i === 0 && arguments.length === 1) ? v : callbackfn(value, v, i, this._ite)
+    return value
+  }
+
+  reduceRight (callbackfn, initialValue = undefined) {
+    return toArray(this._ite).reduceRight(...arguments)
+  }
+
+  slice (start = 0, end = Number.MAX_SAFE_INTEGER) {
+    return new ArrayIterator(
+      start < 0 || end < 0
+        ? toArray(this._ite).slice(...arguments)
+        : (function * (ite, i) {
+            for (const v of ite) {
+              if (++i >= end) return
+              if (i >= start) yield v
+            }
+          }(this._ite, -1))
+    )
+  }
+
+  some (callbackfn, thisArg = null) {
+    return this.map(callbackfn, thisArg).map(v => !!v).includes(true)
+  }
+
+  toString () {
+    return this.join('')
+  }
+
+  values () {
+    return new ArrayIterator(this._ite)
+  }
+
+  [Symbol.iterator] () {
+    return this._ite[Symbol.iterator]()
+  }
+}
+
+export default function seq (iterator) {
+  if (typeof iterator === 'number') {
+    return new ArrayIterator(Array(iterator).keys())
+  } else if (typeof iterator[Symbol.iterator] === 'function') {
+    return new ArrayIterator(iterator)
+  } else {
+    throw new TypeError('The given argument is not a number or iterable.')
+  }
+}
